eda3f8574cb59da6dbc89afb2b01000a1c67b037
alisa4y <alisa4y@gmail.com>
2021-08-17 08:37:45 +0430

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: "ts-loader",
+            exclude: /node_modules/,
+        }, {
+            test: /\.js$/,
+            enforce: "pre",
+            use: ["source-map-loader"],
+        }, ],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+};
